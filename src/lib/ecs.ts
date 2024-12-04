bcb1cea00a586d01fbec220cddb3d6e9c4f99b46
f-irac-odes <fira.official888@gmail.com>
2024-12-04 16:03:54 +0100

    🤗 fixed lifelong errors in the architecture of the game engine

--- src/lib/ecs.ts
+++ src/lib/ecs.ts
@@ -1,59 +1,105 @@
-import { Collection, type AnyEntity } from './collection';
-import type { IComponentValue, Component } from './components';
-import { Runner } from './Runner';
-import type { ISystem } from './System';
+type IGameEvent<T> = {name: string, payload: T}
 
-type EventC<T> = (e: T) => void;
+type Entity = { id: string; [key: string]: any };
 
-export class World<T extends AnyEntity> extends Collection<T> {
-	added: Array<EventC<Partial<T>>> = [];
-	removed: Array<EventC<Partial<T>>> = [];
-	_runner: Runner<Partial<T>> = new Runner();
+type QueryPredicate<T> = (entity: Partial<T>) => boolean;
 
-	add<V extends IComponentValue>(
-		entity: Partial<T>,
-		component: Component<V> | keyof T,
-		value?: T[keyof T] | undefined
-	): void {
-		super.add(entity, component, value);
-		this.added.forEach((listener) => listener(entity));
-	}
+type EventContext<T extends Entity, P> = {
+  world: World<T>;
+  entity: Partial<T>;
+  payload: P;
+};
 
+type Condition<T, P = any> = (context: EventContext<T, P>) => boolean;
 
-	destroy(entity: Partial<T>): void {
-		super.destroy(entity);
-		this.removed.forEach((listener) => listener(entity));
-	}
+type Action<T, P = any> = (context: EventContext<T, P>) => void;
 
+type IEvent<T, P = any> = {
+  name: string;
+  priority?: number; // Higher priority executes earlier
+  conditions?: Condition<T, P>[];
+  actions: Action<T, P>[];
+};
 
-	on = {
-		entityAdded: (callback: EventC<Partial<T>>) => {
-			this.added.push(callback);
-		},
-		entityRemoved: (callback: EventC<Partial<T>>) => {
-			this.removed.push(callback);
-		}
-	};
+type Middleware<T extends Entity> = (event: IGameEvent<any>, world: World<T>, next: () => void) => void;
 
+class World<T extends Entity> {
+  private entities: Array<Partial<T>> = [];
+  private eventListeners: Record<string, IEvent<T, any>[]> = {};
+  private middlewares: Middleware<T>[] = [];
 
-	run(dt: number): void {
-		this._runner.update(dt, this._entities);
-	}
+  /**
+   * Add an entity to the world.
+   */
+  create(entity: Partial<T>): Partial<T> {
+    if (!entity.id) entity.id = Math.random().toString(36).substring(2); // Generate unique ID
+    this.entities.push(entity);
+    this.emitEvent({ name: "entityCreated", payload: entity });
+    return entity;
+  }
 
+  /**
+   * Remove an entity from the world.
+   */
+  destroy(entity: Partial<T>): void {
+    const index = this.entities.indexOf(entity);
+    if (index !== -1) {
+      this.entities.splice(index, 1);
+      this.emitEvent({ name: "entityDestroyed", payload: entity });
+    }
+  }
 
-	addSystem(system: ISystem<Partial<T>>): void {
-		this._runner.addSystem(system);
-	}
+  /**
+   * Add an event listener.
+   */
+  on<P>(eventName: string, event: IEvent<T, P>): void {
+    if (!this.eventListeners[eventName]) {
+      this.eventListeners[eventName] = [];
+    }
+    this.eventListeners[eventName].push(event);
+    this.eventListeners[eventName].sort((a, b) => (b.priority || 0) - (a.priority || 0)); // Sort by priority
+  }
 
-	removeSystem(system: ISystem<Partial<T>>) {
-		this._runner.removeSystem(system);
-	}
+  /**
+   * Emit an event.
+   */
+  emitEvent<P>(event: IGameEvent<P>): void {
+    const processMiddlewares = (index: number) => {
+      if (index >= this.middlewares.length) {
+        this.processEvent(event);
+      } else {
+        this.middlewares[index](event, this, () => processMiddlewares(index + 1));
+      }
+    };
+    processMiddlewares(0);
+  }
 
-	get entities() {
-		return this._entities;
-	}
+  /**
+   * Process an event.
+   */
+  private processEvent<P>(event: IGameEvent<P>): void {
+    const listeners = this.eventListeners[event.name] || [];
+    listeners.forEach((evt) => {
+      this.entities.forEach((entity) => {
+        const context = { world: this, entity, payload: event.payload };
+        if (evt.conditions?.every((cond) => cond(context))) {
+          evt.actions.forEach((action) => action(context));
+        }
+      });
+    });
+  }
 
-	get runner() {
-		return this._runner;
-	}
+  /**
+   * Add middleware.
+   */
+  use(middleware: Middleware<T>): void {
+    this.middlewares.push(middleware);
+  }
+
+  /**
+   * Query entities based on a predicate.
+   */
+  queryEntities(predicate: QueryPredicate<T>): Array<Partial<T>> {
+    return this.entities.filter(predicate);
+  }
 }
